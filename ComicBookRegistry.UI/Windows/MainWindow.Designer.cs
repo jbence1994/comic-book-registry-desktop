f544693e151e60e1f904d8df99d10a008e62d304
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-05-29 16:06:27 +0200

    Rename button

--- ComicBookRegistry.UI/Windows/MainWindow.Designer.cs
+++ ComicBookRegistry.UI/Windows/MainWindow.Designer.cs
@@ -28,19 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.buttonAddComic = new System.Windows.Forms.Button();
+            this.buttonAddComicBook = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // buttonAddComic
+            // buttonAddComicBook
             // 
-            this.buttonAddComic.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.buttonAddComic.Location = new System.Drawing.Point(12, 674);
-            this.buttonAddComic.Name = "buttonAddComic";
-            this.buttonAddComic.Size = new System.Drawing.Size(227, 43);
-            this.buttonAddComic.TabIndex = 0;
-            this.buttonAddComic.Text = "Képregény hozzáadása";
-            this.buttonAddComic.UseVisualStyleBackColor = true;
-            this.buttonAddComic.Click += new System.EventHandler(this.ButtonAddComic_Click);
+            this.buttonAddComicBook.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonAddComicBook.Location = new System.Drawing.Point(12, 674);
+            this.buttonAddComicBook.Name = "buttonAddComicBook";
+            this.buttonAddComicBook.Size = new System.Drawing.Size(227, 43);
+            this.buttonAddComicBook.TabIndex = 0;
+            this.buttonAddComicBook.Text = "Képregény hozzáadása";
+            this.buttonAddComicBook.UseVisualStyleBackColor = true;
+            this.buttonAddComicBook.Click += new System.EventHandler(this.ButtonAddComic_Click);
             // 
             // MainWindow
             // 
@@ -48,7 +48,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1008, 729);
-            this.Controls.Add(this.buttonAddComic);
+            this.Controls.Add(this.buttonAddComicBook);
             this.Name = "MainWindow";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Képregény nyilvántartó";
@@ -58,6 +58,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Button buttonAddComic;
+        private System.Windows.Forms.Button buttonAddComicBook;
     }
 }
