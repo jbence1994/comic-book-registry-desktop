49b04ae81924ac6e3b03acdb7b5470cb1bc80be7
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-05-29 15:26:35 +0200

    Add file related exceptions

---
+++ ComicBookRegistry.Domain/Exceptions/InvalidFileTypeException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ComicBookRegistry.Domain.Exceptions
+{
+    public class InvalidFileTypeException : Exception
+    {
+        public InvalidFileTypeException()
+            : base("Invalid file type.")
+
+        {
+        }
+    }
+}
