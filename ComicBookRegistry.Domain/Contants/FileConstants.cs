afeed40b48c3b70f9eab0a31b4723979b6910484
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-05-29 15:23:26 +0200

    Add file constants

---
+++ ComicBookRegistry.Domain/Contants/FileConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace ComicBookRegistry.Domain.Contants
+{
+    public static class FileConstants
+    {
+        public const string ComicPhotoUploadsDirectoryPath = "uploads/comics";
+        public const int MaxFileBytes = 10485760;
+        public const string Jpg = ".jpg";
+        public const string Jpeg = ".jpeg";
+        public const string Png = ".png";
+        public const string Bmp = ".bmp";
+        public const string Image = "image";
+    }
+}
