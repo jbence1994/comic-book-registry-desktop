de9092b16567955944a2f01d8b135539cc5469af
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-05-29 16:31:03 +0200

    Add repository interface

---
+++ ComicBookRegistry.Core/Repositories/IComicBookRepository.cs
@@ -0,0 +1,9 @@
+﻿using ComicBookRegistry.Core.Models;
+
+namespace ComicBookRegistry.Core.Repositories
+{
+    public interface IComicBookRepository
+    {
+        ComicBook GetComicBook(int id);
+    }
+}
