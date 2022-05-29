ac4457a3714207b6349ed5be60bcd2baef8d0df9
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-05-29 16:30:52 +0200

    Add more exceptions

---
+++ ComicBookRegistry.Domain/Exceptions/ComicBookPhotoNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComicBookRegistry.Domain.Exceptions
+{
+    public class ComicBookPhotoNotFoundException : Exception
+    {
+        public ComicBookPhotoNotFoundException(string fileName)
+            : base($"Photo of comic book not found with file name: {fileName}.")
+        {
+        }
+    }
+}
