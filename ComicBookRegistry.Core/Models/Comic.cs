b0049bcfd644a03486cfcec8fa365d2300a9ad00
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-05-29 15:36:02 +0200

    Add model classes

---
+++ ComicBookRegistry.Core/Models/Comic.cs
@@ -0,0 +1,10 @@
+﻿namespace ComicBookRegistry.Core.Models
+{
+    public class Comic
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public Photo Photo { get; set; }
+    }
+}
---
+++ ComicBookRegistry.Core/Models/Photo.cs
@@ -0,0 +1,8 @@
+﻿namespace ComicBookRegistry.Core.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+    }
+}
