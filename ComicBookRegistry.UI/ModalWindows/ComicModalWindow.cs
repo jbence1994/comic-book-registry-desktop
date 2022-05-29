962916bfe83e48b2a4528203143844ba1bc6cfe7
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2022-05-29 15:02:38 +0200

    Design ComicModalWindow (#5)
    * Add modal window
    
    * Add white back color
    
    * Add title
    
    * Resize to 720x576
    
    * Center to center screen
    
    * Clean up unused imports
    
    * Register modal window to the service container
    
    * Add image box with initial image
    
    * Add event for uploading a new comic book cover
    
    * Add groupbox for comic book data
    
    * Rename component
    
    * Transfor window to a fixed tool window
    
    * Clear window title
    
    * Change back window when starting app
    
    * Update sizes of the font on groupbox and add first textbox
    
    * Disable window maximization
    
    * Increase font size and add save button
    
    * Add some log
    
    * Add second textbox
    
    * Update start window
    
    * Update tab indexes
    
    * Add cancel button with log
    
    * Add mainwindow to start

---
+++ ComicBookRegistry.UI/ModalWindows/ComicModalWindow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ComicBookRegistry.UI.ModalWindows
+{
+    public partial class ComicModalWindow : Form
+    {
+        public ComicModalWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PictureBoxComicBookCover_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Uploading new image for comic book cover.");
+        }
+
+        private void ButtonSave_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Saving comic book data to database.");
+        }
+
+        private void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Cancelling process.");
+        }
+    }
+}
