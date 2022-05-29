a5272d1e8ca1d6a99e9192426fe90ccce0b16b21
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2022-05-29 13:53:24 +0200

    Implement dependency injection (#3)
    * Install necessary packages to implement Microsoft based DI
    
    * Wire up service container

--- ComicBookRegistry.UI/Application.cs
+++ ComicBookRegistry.UI/Application.cs
@@ -1,4 +1,6 @@
 using ComicBookRegistry.UI.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Windows.Forms;
 
@@ -9,10 +11,17 @@ namespace ComicBookRegistry.UI
         [STAThread]
         public static void Main()
         {
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.AddScoped<MainWindow>();
+                })
+                .Build();
+
             System.Windows.Forms.Application.SetHighDpiMode(HighDpiMode.SystemAware);
             System.Windows.Forms.Application.EnableVisualStyles();
             System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
-            System.Windows.Forms.Application.Run(new MainWindow());
+            System.Windows.Forms.Application.Run(host.Services.GetRequiredService<MainWindow>());
         }
     }
 }
