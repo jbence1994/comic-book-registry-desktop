7eaefdabdda82cd6bbcc6e104ab6b719a80fd6d3
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-06-04 20:36:48 +0200

    Unable to fake a FileInfo object (no setters for validation properties) -> Need to create a custom type

---
+++ ComicBookRegistry.Domain.Tests/TestBuilders/FileInfoTestBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace ComicBookRegistry.Domain.Tests.TestBuilders
+{
+    public class FileInfoTestBuilder
+    {
+        public static FileInfo Default => Build();
+        public static FileInfo Null => null;
+        public static FileInfo Empty => Build(length: 0);
+        public static FileInfo Large => Build(length: 10485761);
+        public static FileInfo WithInvalidFileType => Build(fileName: "comic_book_cover.gif");
+
+        private static FileInfo Build(
+            int length = 1,
+            string name = "photoToUpload",
+            string fileName = "comic_book_cover.jpg"
+        )
+        {
+            return new FileInfo(fileName);
+        }
+    }
+}
---
+++ ComicBookRegistry.Domain.Tests/Validation/FileValidatorTests.cs
@@ -0,0 +1,71 @@
+﻿using ComicBookRegistry.Domain.Exceptions;
+using ComicBookRegistry.Domain.Tests.TestBuilders;
+using ComicBookRegistry.Domain.Validation;
+using NUnit.Framework;
+
+namespace ComicBookRegistry.Domain.Tests.Validation
+{
+    [TestFixture]
+    public class FileValidatorTests
+    {
+        private FileValidator _fileValidator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileValidator = new FileValidator();
+        }
+
+        [Test]
+        public void ValidateTest_HappyPath()
+        {
+            var file = FileInfoTestBuilder.Default;
+
+            Assert.DoesNotThrow(() => _fileValidator.Validate(file));
+        }
+
+        [Test]
+        public void ValidateTest_UnhappyPath_ShouldThrowNullFileException()
+        {
+            var file = FileInfoTestBuilder.Null;
+
+            Assert.That(
+                () => _fileValidator.Validate(file),
+                Throws.TypeOf<NullFileException>()
+            );
+        }
+
+        [Test]
+        public void ValidateTest_UnhappyPath_ShouldThrowEmptyFileException()
+        {
+            var file = FileInfoTestBuilder.Empty;
+
+            Assert.That(
+                () => _fileValidator.Validate(file),
+                Throws.TypeOf<EmptyFileException>()
+            );
+        }
+
+        [Test]
+        public void ValidateTest_UnhappyPath_ShouldThrowMaximumFileSizeExceededException()
+        {
+            var file = FileInfoTestBuilder.Large;
+
+            Assert.That(
+                () => _fileValidator.Validate(file),
+                Throws.TypeOf<MaximumFileSizeExceededException>()
+            );
+        }
+
+        [Test]
+        public void ValidateTest_UnhappyPath_ShouldThrowInvalidFileTypeException()
+        {
+            var file = FileInfoTestBuilder.WithInvalidFileType;
+
+            Assert.That(
+                () => _fileValidator.Validate(file),
+                Throws.TypeOf<InvalidFileTypeException>()
+            );
+        }
+    }
+}
