024d2be286e802b57b5dadbd995b8c56a00d2628
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-06-04 15:55:20 +0200

    Add mocked implementation of ComicBookRepository

---
+++ ComicBookRegistry.Core/Repositories/MockComicBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using ComicBookRegistry.Core.Models;
+
+namespace ComicBookRegistry.Core.Repositories
+{
+    public class MockComicBookRepository : IComicBookRepository
+    {
+        public ComicBook GetComicBook(int id)
+        {
+            return new ComicBook
+            {
+                Id = id,
+                Title = "Marvel Annual #1",
+                ISSN = "0000-0000",
+                Photo = null
+            };
+        }
+    }
+}
